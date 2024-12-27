7a94c333237a3db70b6644c000817641a7ef6257
4242vfwevrevwerv <glasnak.mmichal@gmail.com>
2024-12-27 15:08:39 +0100

    Fixed bug with not loading config

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
-# config.py
+# globals.config.py
 from constants import CONFIG_PATH, SAVE_CREDS_WARNING, SAVED_CREDS_PATH
 from utils import clean_path
+import globals
 
 import ast
 import os
 
 def read_config():
-    global config
     try:
         with open(CONFIG_PATH, "r") as config_file:
             config_str = config_file.read().strip()
     except FileNotFoundError:
         return False
     if config_str:
-        config = ast.literal_eval(config_str)
+        globals.config = ast.literal_eval(config_str)
         return True
     return False
     
@@ -23,28 +23,25 @@ def write_config(config):
         config_file.write(str(config))
 
 def remember_creds():
-    global config
-    config["remember_creds"] = True
+    globals.config["remember_creds"] = True
     
-    write_config(config)
+    write_config(globals.config)
     print(SAVE_CREDS_WARNING)
 
 def forget_and_remove():
-    global config
-    config["remember_creds"] = False
+    globals.config["remember_creds"] = False
     os.remove(SAVED_CREDS_PATH)
 
-    write_config(config)
+    write_config(globals.config)
     print("You've been successfully forgotten.")
 
 def set_home(new_home = None):
     if not new_home:
         return
-    global config
     new_home = clean_path(new_home)
-    config["home"] = new_home
+    globals.config["home"] = new_home
 
-    write_config(config)
+    write_config(globals.config)
     print(f"HOME = {new_home}")
 
 def alias(argv1):
@@ -54,10 +51,9 @@ def alias(argv1):
         print("man alias")
         return
 
-    global config
-    config["aliases"][alias] = alias_cmd
+    globals.config["aliases"][alias] = alias_cmd
 
-    write_config(config)
+    write_config(globals.config)
     print(f"Typing {alias} triggers {alias_cmd}.")
 
 
